eb96184095accab674bcf3904d993d687c06f40e
Oleksandr Sulyma <sulima.ab@gmail.com>
2026-03-27 02:41:31 +0200

    Update api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
+const publicUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const baseURL = `${publicUrl}/api`;
 
 export const nextServer = axios.create({
   baseURL,
